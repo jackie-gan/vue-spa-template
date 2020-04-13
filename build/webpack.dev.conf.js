217396c2255f9122394dd047d92578bf8a1a5030
ganzhanjian <ganzhanjian@mucfc.com>
2020-04-13 09:47:48 +0800

    chore: change host to 127.0.0.1

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -18,7 +18,7 @@ module.exports = webpackMerge(baseConfig, {
     publicPath: '/',
     disableHostCheck: true,
     quiet: false,
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: 3000,
     open: true,
     stats: {
